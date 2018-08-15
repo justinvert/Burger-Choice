aa30ea49627ca624851b01839d0f9f7044c8b32c
Justin V <justinvuser@gmail.com>
2018-08-15 14:30:30 -0400

    Server.js and db folder updated

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Triple Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', false);
+
