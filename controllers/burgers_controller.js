df9ec015e312ce2e1c09df330122efbefbe25ff5
Justin V <justinvuser@gmail.com>
2018-08-17 09:38:20 -0400

    orm.js and burgers_controller.js updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,16 @@
 var express = require("express");
 
 var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+      };
+      res.render("index", hbsObject);
+    });
+  });
+
+  module.exports = router;
