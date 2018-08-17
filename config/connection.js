f62ef658c317c9a0ba08dbe99a9fefa5f988cd12
Justin V <justinvuser@gmail.com>
2018-08-17 17:35:49 -0400

    Multiple files updated, .env added

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
+require('dotenv').config();
+var password = process.env.DB_PASSWORD;
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  
+  password: password,
   database: "burgers_db"
 });
 
 connection.connect(function(err) {
     if (err) {
-      console.error("Unable to currently connect");
+      console.error("Unable to currently connect.\nPlease check your password.");
       return;
  }
     console.log("connected as id " + connection.threadId);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,12 +5,43 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
+      res.redirect("/index");
+  });
+
+router.get("/index", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
       };
+
+      console.log(hbsObject);
       res.render("index", hbsObject);
     });
   });
+
+router.post("/", function(req, res) {
+    burger.insertOne([
+      "name", "devoured"
+    ],
+    [req.body.name, req.body.devoured], function(res) {
+      res.redirect("/");
+    });
+  });
+
+router.put("/:id", function (req,res){
+var condition = "id = " + req.params.id;
+
+burger.update({
+  devoured: req.body.devoured
+}, condition, function(result) {
+  // if (result.changedRows == 0) {
+  //   return res.status(404).end();
+  // } else {
+  //   res.status(200).end();
+  // }
+  res.redirect("/");
+});
+
+});
 
   module.exports = router;
